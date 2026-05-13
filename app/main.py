c2bedbb8ad8447277b8615f8c52925301fc59b94
Vladislav <Vans91471@gmail.com>
2026-05-13 14:16:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:  # 5 usages  new *
+
+    mv, file_name, destination_path = command.split()
+    if mv != "mv" or destination_path[-1] == "/":
+        return
+    new_file = "".join(destination_path.split("/")[-1])
+
+    directory = "/".join(destination_path.split("/")[:-1])
+    if not directory:
+        os.replace(file_name, new_file)
+    else:
+        os.makedirs(directory, exist_ok=True)
+        os.replace(f"{file_name}", f"{directory}/{new_file}")
