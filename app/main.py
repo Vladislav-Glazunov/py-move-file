45259fe1d20ce8ce8159838d58da848fb4ed2a6e
Vladislav <Vans91471@gmail.com>
2026-05-14 22:29:36 +0300

    Solution 2-two

--- app/main.py
+++ app/main.py
@@ -1,16 +1,35 @@
 import os
 
 
-def move_file(command: str) -> None:  # 5 usages  new *
+def move_file(command: str) -> None:
 
-    mv, file_name, destination_path = command.split()
-    if mv != "mv" or destination_path[-1] == "/":
+    command_parts = command.split()
+    if len(command_parts) != 3:
         return
-    new_file = "".join(destination_path.split("/")[-1])
 
-    directory = "/".join(destination_path.split("/")[:-1])
-    if not directory:
-        os.replace(file_name, new_file)
+    mv, file_name, destination_path = command_parts
+    if mv != "mv":
+        return
+    path_parts = destination_path.split("/")
+
+    new_file = path_parts[-1]
+    directory = path_parts[:-1]
+
+    if directory:
+        for index, dir_names in enumerate(directory):
+            current_path = os.path.join(*directory[:index + 1])
+            if not os.path.exists(current_path):
+                os.mkdir(current_path)
+        path_new_file = os.path.join(current_path, new_file)
+        with (
+            open(file_name, "r") as file_in,
+            open(path_new_file, "w") as file_out
+        ):
+            read_content = file_in.read()
+            file_out.write(read_content)
+        os.remove(file_name)
     else:
-        os.makedirs(directory, exist_ok=True)
-        os.replace(f"{file_name}", f"{directory}/{new_file}")
+        with open(file_name, "r") as file_in, open(new_file, "w") as file_out:
+            read_content = file_in.read()
+            file_out.write(read_content)
+        os.remove(file_name)
